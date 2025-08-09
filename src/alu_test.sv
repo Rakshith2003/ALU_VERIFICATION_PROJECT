c7245de096da4b9f85419c144d57558492cc1f8f
Rakshith S Shetty <111422371+Rakshith2003@users.noreply.github.com>
2025-08-09 13:13:04 +0530

    alu_test.sv

--- src/alu_test.sv
+++ src/alu_test.sv
@@ -238,30 +238,39 @@ class alu_test12 extends alu_test;
   endtask
 endclass
 
-class alu_test13 extends alu_test;
- alu_transaction13 trans;
-  function new(virtual alu_inf.DRV drv_vif,virtual alu_inf.MON mon_vif,virtual alu_inf.REF ref_vif);
-    super.new(drv_vif,mon_vif,ref_vif);
-  endfunction
+// class alu_test13 extends alu_test;
+//  alu_transaction13 trans;
+//   function new(virtual alu_inf.DRV drv_vif,virtual alu_inf.MON mon_vif,virtual alu_inf.REF ref_vif);
+//     super.new(drv_vif,mon_vif,ref_vif);
+//   endfunction
 
-  task run();
-    $display("child test");
-    env=new(drv_vif,mon_vif,ref_vif);
-    env.build;
-    begin
-    trans = new();
-    env.gen.blueprint= trans;
-    end
-    env.start;
-  endtask
-endclass
-/*
+//   task run();
+//     $display("child test");
+//     env=new(drv_vif,mon_vif,ref_vif);
+//     env.build;
+//     begin
+//     trans = new();
+//     env.gen.blueprint= trans;
+//     end
+//     env.start;
+//   endtask
+// endclass
+
 class test_regression extends alu_test;
  alu_transaction  trans0;
  alu_transaction1 trans1;
 alu_transaction2 trans2;
  alu_transaction3 trans3;
 alu_transaction4 trans4;
+  alu_transaction5 trans5;
+  alu_transaction6 trans6;
+  alu_transaction7 trans7;
+  alu_transaction8 trans8;
+  alu_transaction9 trans9;
+  alu_transaction10 trans10;
+  alu_transaction11 trans11;
+  alu_transaction12 trans12;
+  
   function new(virtual alu_inf.DRV drv_vif,virtual alu_inf.MON mon_vif,virtual alu_inf.REF ref_vif);
     super.new(drv_vif,mon_vif,ref_vif);
   endfunction
@@ -309,7 +318,61 @@ alu_transaction4 trans4;
     end
     env.start;
 //////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////
+    begin
+    trans5 = new();
+    env.gen.blueprint= trans5;
+    end
+    env.start;
+//////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////
+    begin
+    trans6 = new();
+    env.gen.blueprint= trans6;
+    end
+    env.start;
+//////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////
+    begin
+    trans7 = new();
+    env.gen.blueprint= trans7;
+    end
+    env.start;
+//////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////
+    begin
+    trans8 = new();
+    env.gen.blueprint= trans8;
+    end
+    env.start;
+//////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////
+    begin
+    trans9 = new();
+    env.gen.blueprint= trans9;
+    end
+    env.start;
+//////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////
+    begin
+    trans10 = new();
+    env.gen.blueprint= trans10;
+    end
+    env.start;
+//////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////
+    begin
+    trans11 = new();
+    env.gen.blueprint= trans11;
+    end
+    env.start;
+//////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////
+    begin
+    trans12 = new();
+    env.gen.blueprint= trans12;
+    end
+    env.start;
+//////////////////////////////////////////////////////
   endtask
 endclass
-
-*/
