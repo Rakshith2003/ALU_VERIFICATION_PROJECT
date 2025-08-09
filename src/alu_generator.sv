e11992bb2a244681bff71838835b91e9cbdbf69b
Rakshith S Shetty <111422371+Rakshith2003@users.noreply.github.com>
2025-08-09 13:09:31 +0530

    alu_generator.sv

--- src/alu_generator.sv
+++ src/alu_generator.sv
@@ -16,7 +16,7 @@ class alu_generator;
       begin
       assert(blueprint.randomize()==1);
       mbx_gd.put(blueprint.copy());
-      $display(" ( GENERATOR ) has randomized the transaction and sent to driver through mailbox\n OPA=%d|OPB=%d | CE=%d | MODE=%d | CIN =%d | INP_VALID =%b | CMD=%b |",blueprint.OPA,blueprint.OPB,blueprint.CE,blueprint.MODE,blueprint.CIN,blueprint.INP_VALID,blueprint.CMD);
+        $display(" [ GENERATOR : @%t] has randomized the transaction and sent to driver through mailbox\n OPA=%d|OPB=%d | CE=%d | MODE=%d | CIN =%d | INP_VALID =%b | CMD=%b |",$time,blueprint.OPA,blueprint.OPB,blueprint.CE,blueprint.MODE,blueprint.CIN,blueprint.INP_VALID,blueprint.CMD);
         $display("\n");
       end
       end
